6b5bf3a5a09cb9dd5e4406c714d0f21f83e8e126
Aparna Joshi <aparnarenujoshi@gmail.com>
2026-01-18 06:27:36 +0530

    First commit

---
+++ cashcard/src/main/java/com/cashcard/CashcardApplicationForFamilyCashCardApplication.java
@@ -0,0 +1,13 @@
+package com.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardApplicationForFamilyCashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcardApplicationForFamilyCashCardApplication.class, args);
+	}
+
+}
